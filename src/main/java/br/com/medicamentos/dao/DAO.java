7bc6210bfaf3a1b80a8699ee54e2f871087c1054
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-11-21 23:13:51 -0300

    Adicionada classe dao

--- src/main/java/br/com/medicamentos/dao/DAO.java
+++ src/main/java/br/com/medicamentos/dao/DAO.java
@@ -1,5 +1,17 @@
 package br.com.medicamentos.dao;
 
-public class DAO {
+import java.io.Serializable;
+
+import javax.persistence.EntityManager;
+
+import br.com.medicamentos.model.Base;
+
+public class DAO<T extends Base> implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private static EntityManager manager = ConnectionFactory.getEntityManager();
+	
+	
 
 }
